dc07179bdfd7f80311f03b0b2aa636df34d5084b
Hong Baojin <13809735212.163.com>
2020-08-25 18:22:13 +0800

    based finished

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
 charset = 'UTF-8';
 $(function(){
-  
+  function tab(btn, cont, classname){
+    $(btn).each(function(i){
+      $(this).mouseenter(function(){
+        $(this).addClass(classname).siblings().removeClass(classname);
+        $($(cont)[i]).addClass(classname).siblings().removeClass(classname);
+      });
+    });
+  }
+  tab('.a-nav li', '.a-cont .c-item', 'on');
 });
